7bb3227756cd18e83526de0c35c2b0538564dde8
Black <756319278@qq.com>
2017-11-20 17:46:28 +0800

    add templeate

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'iview/dist/styles/iview.css';
+import Axios from 'axios'
+const iview = require('iview')
+
+Vue.use(iview)
+
+Vue.prototype.$http = Axios;
+Vue.config.productionTip = false
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
